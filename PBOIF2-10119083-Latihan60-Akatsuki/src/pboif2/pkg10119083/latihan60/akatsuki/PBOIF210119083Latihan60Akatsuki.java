7fd980e60bf606f4a9d0b994473669c200c15a5c
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-11-22 01:31:46 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan60-Akatsuki/src/pboif2/pkg10119083/latihan60/akatsuki/PBOIF210119083Latihan60Akatsuki.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan60.akatsuki;
+
+/**
+ *
+ * NAMA        : Rafsam Zen Mustaofa
+ * KELAS       : PBOIF2
+ * NIM         : 10119083
+ * Deskripsi   : program Akatsuki
+ */
+public class PBOIF210119083Latihan60Akatsuki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Anggota a = new Anggota();
+        a.tampilProfil();
+    }
+    
+}
